00a81e4b0f269244e9434f3462931412cc828b3f
1029458906@qq.com <1029458906@qq.com>
2018-05-14 12:38:47 +0800

    delete unuseful

--- common.bak.bak.c
+++ common.bak.bak.c
@@ -90,11 +90,53 @@ static int vminfo_fd = -1;
 #define USRNAMSIZ  GETBUFSIZ
 #define ROWBUFSIZ  SCREENMAX + CLRBUFSIZ
 
+enum pflag {
+   P_PID, P_PPD, P_URR, P_UID, P_URE, P_GRP, P_TTY,
+   P_PRI, P_NCE,
+   P_CPN, P_CPU, P_TME, P_TM2,
+   P_MEM, P_VRT, P_SWP, P_RES, P_COD, P_DAT, P_SHR,
+   P_FLT, P_DRT,
+   P_STA, P_CMD, P_WCH, P_FLG
+};
+
 #define PUTT(fmt,arg...) do { \
       char _str[ROWBUFSIZ]; \
       snprintf(_str, sizeof(_str), fmt, ## arg); \
       putp(_str); \
    } while (0)
+
+#define PUFF(fmt,arg...)                               \
+do {                                                     \
+   char _str[ROWBUFSIZ];                                   \
+   char *_ptr;                                               \
+   int _len = 1 + snprintf(_str, sizeof(_str), fmt, ## arg);   \
+   if (Batch) _ptr = _str;                                   \
+   else {                                                 \
+      _ptr = &Pseudo_scrn[Pseudo_row++ * Pseudo_cols];  \
+      if (memcmp(_ptr, _str, _len)) {                \
+         memcpy(_ptr, _str, _len);                \
+      } else {                                 \
+         _ptr = "\n";                       \
+      }                                 \
+   }                                \
+   putp(_ptr);                   \
+} while (0)
+
+#define DEF_RCFILE { \
+		 0, 1, 3.0, 0, { \
+		 { P_CPU, DEF_WINFLGS, 0, \
+			COLOR_RED, COLOR_RED, COLOR_YELLOW, COLOR_RED, \
+			"Def", DEF_FIELDS }, \
+		 { P_PID, DEF_WINFLGS, 0, \
+			COLOR_CYAN, COLOR_CYAN, COLOR_WHITE, COLOR_CYAN, \
+			"Job", JOB_FIELDS }, \
+		 { P_MEM, DEF_WINFLGS, 0, \
+			COLOR_MAGENTA, COLOR_MAGENTA, COLOR_BLUE, COLOR_MAGENTA, \
+			"Mem", MEM_FIELDS }, \
+		 { P_URE, DEF_WINFLGS, 0, \
+			COLOR_YELLOW, COLOR_YELLOW, COLOR_GREEN, COLOR_YELLOW, \
+			"Usr", USR_FIELDS } \
+		 } }
 
 #define FILE_TO_BUF(filename, fd) do{				\
     static int local_n;						\
@@ -380,6 +422,8 @@ static char str[100] = " ";
 static int proc_table_size;
 
 static int Cpu_tot;
+
+static RCF_t Rc = DEF_RCFILE;
 
 static char buf[2048];
 
@@ -1085,7 +1129,7 @@ static void prochlp (proc_t *this)
       oldtimev.tv_usec = timev.tv_usec;
 
       // if in Solaris mode, adjust our scaling for all cpus
-      //Frame_tscale = 100.0f / ((float)(Hertz) * (float)et * (Rc.mode_irixps ? 1 : Cpu_tot));
+      Frame_tscale = 100.0f / ((float)(Hertz) * (float)et * (Rc.mode_irixps ? 1 : Cpu_tot));
       maxt_sav = Frame_maxtask;
       Frame_maxtask = Frame_running = Frame_sleepin = Frame_stopped = Frame_zombied = 0;
 
@@ -1282,6 +1326,86 @@ static int compare_mem_table_structs(const void *a, const void *b){
   return strcmp(((const mem_table_struct*)a)->name,((const mem_table_struct*)b)->name);
 }
 
+void meminfo(void){
+  char namebuf[16]; /* big enough to hold any row name */
+  mem_table_struct findme = { namebuf, NULL};
+  mem_table_struct *found;
+  char *head;
+  char *tail;
+  static const mem_table_struct mem_table[] = {
+  {"Active",       &kb_active},       // important
+  {"AnonPages",    &kb_anon_pages},
+  {"Bounce",       &kb_bounce},
+  {"Buffers",      &kb_main_buffers}, // important
+  {"Cached",       &kb_main_cached},  // important
+  {"CommitLimit",  &kb_commit_limit},
+  {"Committed_AS", &kb_committed_as},
+  {"Dirty",        &kb_dirty},        // kB version of vmstat nr_dirty
+  {"HighFree",     &kb_high_free},
+  {"HighTotal",    &kb_high_total},
+  {"Inact_clean",  &kb_inact_clean},
+  {"Inact_dirty",  &kb_inact_dirty},
+  {"Inact_laundry",&kb_inact_laundry},
+  {"Inact_target", &kb_inact_target},
+  {"Inactive",     &kb_inactive},     // important
+  {"LowFree",      &kb_low_free},
+  {"LowTotal",     &kb_low_total},
+  {"Mapped",       &kb_mapped},       // kB version of vmstat nr_mapped
+  {"MemFree",      &kb_main_free},    // important
+  {"MemShared",    &kb_main_shared},  // important, but now gone!
+  {"MemTotal",     &kb_main_total},   // important
+  {"NFS_Unstable", &kb_nfs_unstable},
+  {"PageTables",   &kb_pagetables},   // kB version of vmstat nr_page_table_pages
+  {"ReverseMaps",  &nr_reversemaps},  // same as vmstat nr_page_table_pages
+  {"SReclaimable", &kb_swap_reclaimable}, // "swap reclaimable" (dentry and inode structures)
+  {"SUnreclaim",   &kb_swap_unreclaimable},
+  {"Slab",         &kb_slab},         // kB version of vmstat nr_slab
+  {"SwapCached",   &kb_swap_cached},
+  {"SwapFree",     &kb_swap_free},    // important
+  {"SwapTotal",    &kb_swap_total},   // important
+  {"VmallocChunk", &kb_vmalloc_chunk},
+  {"VmallocTotal", &kb_vmalloc_total},
+  {"VmallocUsed",  &kb_vmalloc_used},
+  {"Writeback",    &kb_writeback},    // kB version of vmstat nr_writeback
+  };
+  const int mem_table_count = sizeof(mem_table)/sizeof(mem_table_struct);
+
+  FILE_TO_BUF(MEMINFO_FILE,meminfo_fd);
+
+  kb_inactive = ~0UL;
+
+  head = buf;
+  for(;;){
+    tail = strchr(head, ':');
+    if(!tail) break;
+    *tail = '\0';
+    if(strlen(head) >= sizeof(namebuf)){
+      head = tail+1;
+      goto nextline;
+    }
+    strcpy(namebuf,head);
+    found = bsearch(&findme, mem_table, mem_table_count,
+        sizeof(mem_table_struct), compare_mem_table_structs
+    );
+    head = tail+1;
+    if(!found) goto nextline;
+    *(found->slot) = strtoul(head,&tail,10);
+nextline:
+    tail = strchr(head, '\n');
+    if(!tail) break;
+    head = tail+1;
+  }
+  if(!kb_low_total){  /* low==main except with large-memory support */
+    kb_low_total = kb_main_total;
+    kb_low_free  = kb_main_free;
+  }
+  if(kb_inactive==~0UL){
+    kb_inactive = kb_inact_dirty + kb_inact_clean + kb_inact_laundry;
+  }
+  kb_swap_used = kb_swap_total - kb_swap_free;
+  kb_main_used = kb_main_total - kb_main_free;
+}
+
 static void show_special(int interact, const char *glob){
 	char *line_end, line[2048];
 	long long cols;
@@ -1289,7 +1413,8 @@ static void show_special(int interact, const char *glob){
 		cols = line_end - glob;
 		memcpy(line, glob, cols);
 		line[cols] = '\0';
-		PUTT("%s%s\n", line, clr_eol);
+		if (interact) PUTT("%s%s\n", line, clr_eol);
+      	else PUFF("%s%s\n", line, clr_eol);
 		glob = line_end + 1;
 	}
 	if (*glob) PUTT("%s", glob);
@@ -1318,6 +1443,57 @@ static proc_t **summary_show (void){
 
 	return p_table;
 }	
+
+static void old_Hertz_hack(void){
+	unsigned long long user_j, nice_j, sys_j, other_j;  /* jiffies (clock ticks) */
+  double up_1, up_2, seconds;
+  unsigned long long jiffies;
+  unsigned h;
+  char *__restrict savelocale;
+
+  savelocale = setlocale(LC_NUMERIC, NULL);
+  setlocale(LC_NUMERIC, "C");
+  do{
+    FILE_TO_BUF(UPTIME_FILE,uptime_fd);  sscanf(buf, "%lf", &up_1);
+    /* uptime(&up_1, NULL); */
+    FILE_TO_BUF(STAT_FILE,stat_fd);
+    sscanf(buf, "cpu %Lu %Lu %Lu %Lu", &user_j, &nice_j, &sys_j, &other_j);
+    FILE_TO_BUF(UPTIME_FILE,uptime_fd);  sscanf(buf, "%lf", &up_2);
+    /* uptime(&up_2, NULL); */
+  } while((long long)( (up_2-up_1)*1000.0/up_1 )); /* want under 0.1% error */
+  setlocale(LC_NUMERIC, savelocale);
+  jiffies = user_j + nice_j + sys_j + other_j;
+  seconds = (up_1 + up_2) / 2;
+  h = (unsigned)( (double)jiffies/seconds/smp_num_cpus );
+  /* actual values used by 2.4 kernels: 32 64 100 128 1000 1024 1200 */
+  switch(h){
+  case    9 ...   11 :  Hertz =   10; break; /* S/390 (sometimes) */
+  case   18 ...   22 :  Hertz =   20; break; /* user-mode Linux */
+  case   30 ...   34 :  Hertz =   32; break; /* ia64 emulator */
+  case   48 ...   52 :  Hertz =   50; break;
+  case   58 ...   61 :  Hertz =   60; break;
+  case   62 ...   65 :  Hertz =   64; break; /* StrongARM /Shark */
+  case   95 ...  105 :  Hertz =  100; break; /* normal Linux */
+  case  124 ...  132 :  Hertz =  128; break; /* MIPS, ARM */
+  case  195 ...  204 :  Hertz =  200; break; /* normal << 1 */
+  case  247 ...  252 :  Hertz =  250; break;
+  case  253 ...  260 :  Hertz =  256; break;
+  case  393 ...  408 :  Hertz =  400; break; /* normal << 2 */
+  case  790 ...  808 :  Hertz =  800; break; /* normal << 3 */
+  case  990 ... 1010 :  Hertz = 1000; break; /* ARM */
+  case 1015 ... 1035 :  Hertz = 1024; break; /* Alpha, ia64 */
+  case 1180 ... 1220 :  Hertz = 1200; break; /* Alpha */
+  default:
+#ifdef HZ
+    Hertz = (unsigned long long)HZ;    /* <asm/param.h> */
+#else
+    /* If 32-bit or big-endian (not Alpha or ia64), assume HZ is 100. */
+    Hertz = (sizeof(long)==sizeof(int) || htons(999)==999) ? 100UL : 1024UL;
+#endif
+    fprintf(stderr, "Unknown HZ value! (%d) Assume %Ld.\n", h, Hertz);
+  }
+}
+
 
 void init(void){
 	old_Hertz_hack()
